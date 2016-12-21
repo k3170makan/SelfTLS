ab834ee99661993d06ebd2802b6637cb33a8b227
Keith Makan <keithmakan@Keiths-MacBook.local>
2016-12-22 01:22:29 +0200

    progress on des implementation

--- crypt/des.c
+++ crypt/des.c
@@ -63,14 +63,14 @@ static void permute ( unsigned char target[], //target array in which the result
 }
 static void expansion_permutation(unsigned char target[], const unsigned char src[]){
 	static const unsigned char e_table[] = {
-		32 , 1 , 2 , 3 , 4 , 5,
-		4 , 5 , 6 , 7 , 8 , 9,
-		8 , 9 , 10 , 11 , 12 , 13,
-		12 , 13 , 14 , 15 , 16 , 17,
-		16 , 17 , 18 , 19 , 20 , 21,
-		20 , 21 , 22 , 23 , 24 , 25,
-		24 , 25 , 26 , 27 , 28 , 29,
-		28 , 29 , 30 , 31 , 32 , 1
+		32, 1,   2,  3,  4,  5,
+		 4, 5,   6,  7,  8,  9,
+		 8, 9,  10, 11, 12, 13,
+		12, 13, 14, 15, 16, 17,
+		16, 17, 18, 19, 20, 21,
+		20, 21, 22, 23, 24, 25,
+		24, 25, 26, 27, 28, 29,
+		28, 29, 30, 31, 32,  1
 	};
 	permute(target,src,e_table,sizeof(e_table));
 	return;
@@ -118,7 +118,14 @@ static void key_permutation_1(unsigned char target[],
 static void key_permutation_2(unsigned char target[], 
 											unsigned char src[]){
 	const unsigned char pc2[] = {
-	
+		14, 17, 11, 24,  1,  5,
+		 3, 28, 15,  6, 21, 10,
+		23, 19, 12,  4, 26,  8,
+		16,  7, 27, 20, 13,  2,
+		41, 52, 31, 37, 47, 55,
+		30, 40, 51, 45, 33, 48,
+		44, 49, 39, 56, 34, 53,
+		46, 42, 50, 36, 29, 32	
 	};
 	permute(target,src,pc2,sizeof(pc2));
 }
